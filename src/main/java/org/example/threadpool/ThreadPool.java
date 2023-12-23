658549b733d9759c75aa44f0efed1566f05f8a07
GangminRYOU <dbrkdals1994@gmail.com>
2023-12-23 12:14:55 +0900

    feat(12/23) : ThreadPool 개선 - Interrupt 대신 SHUTDOWN_TASK를 통해 ThreadPool 종료 signaling - shutdown 여러 번 호출 방지를 위한 AtomicBoolean 적용 - SHUTDOWN_TASK 적용 이후, 불필요한 queue.isEmpty()와 shutdown 여부 확인 리팩토링

--- src/main/java/org/example/threadpool/ThreadPool.java
+++ src/main/java/org/example/threadpool/ThreadPool.java
@@ -3,6 +3,7 @@ package org.example.threadpool;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ThreadPool implements Executor {
@@ -10,11 +11,40 @@ public class ThreadPool implements Executor {
     /**
      * TODO: LinkedTransferQueue에 대한 조사 필요
      */
+
+    /**
+     * {@link #ThreadPool(int)}
+     * 주기적으로 Interrupt를 보내는 것은 좋지 않은 구현이다.
+     * Interrupt가 아닌 Signal을 보낼 방법이 필요하다.
+     * => 모든 Thread가 최소 1개의 Job을 가지게 하면 된다.
+     * {@link java.util.LinkedList} 에 나오는 Terminator와 유사
+     */
+    private static final Runnable SHUTDOWN_TASK = () -> {};
     private final BlockingQueue<Runnable> queue = new LinkedTransferQueue<>();
 
     private Thread[] threads;
     private AtomicBoolean started = new AtomicBoolean();
+    /**
+     * 정상 종료를 위한 flag
+     * JVM이 CPU core에 캐싱하면 메모리 동기화가 안될 수 있다.
+     * 즉, core마다 서로 다른 state를 가지고 있을 수 있다.
+     */
+    private AtomicBoolean shutdown = new AtomicBoolean();
+    /**
+     * shutdown이 여러 번 호출 되는 것에 대한 방어를 위해 Atomic Variable사용
+     */
 
+    /**
+     * 이렇게 생성하게 되면, 할일이 적든 많든 간에 Thread가 지정한 만큼 생기게 된다.
+     * 만약 1000개의 스레드를 생성하는데 2개의 할일만 있다면, 자원이 낭비된다.
+     */
+
+    /** (12/16 - 원시 ThreadPool을 개선하기)
+     * 1. ThreadPool을 정지하는 기능이 없다.
+     * 2. ThreadPool의 Thread를 필요한 만큼 만들지 않고 무조건 최대한으로 만든다.
+     * @param numOfThreads
+     * Thread Pool같은 경우에 동시성 이슈가 항상 발생할 수 있기 때문에, 주의해야 한다.
+     */
     public ThreadPool(int numOfThreads){
         threads = new Thread[numOfThreads];
         for (int i = 0; i < numOfThreads; i++) {
@@ -22,16 +52,58 @@ public class ThreadPool implements Executor {
                 //...
                 //Take의 경우 Blocking 형태로 무한정 기다리게 된다.
                 //Queue에 작업이 들어오면, Thread가 대기하다가 실행하는 구조
-                try {
+                //try {
                     //Task를 지속적으로 받아서 수행해야하니까 무한 루프를 돌아야 한다.
-                    for(;;){
+                    //shutdown이 아닐때만 job을 take할수 있게 수정한다.
+                /**
+                 * shutdown일 때만 shutdown을 하는게 아니라, Queue에 Job이 없을떄만 Shutdown을 한다.
+                 * shutdown이고, queue에 Job이 없을때만 반복한다.
+                 * @implNote 하지만 문제가 있다. -> Thread 1과 Thread 2가 job이 queue에 하나 있을때,
+                 * 둘다 queue가 비지 않았다고 여긴다. -> queue.take()
+                 * 첫번째 Thread는 Job을 끝내겠지만, Job을 받지 못한 두번째 Thread는 interrupt를
+                 * 해주지 않으면 계속 기다리게 된다.
+                 * -> 두가지 방법이 있다.
+                 * 1. 반복적으로 interrupt를 해서 Thread를 깨우기
+                 * 2. 정해진 시간 정도만 queue.take()로 기다리기
+                 */
+                // 사용자가 무작위 하게 Queue에 넣는 job에서 InterruptedException을 발생시켜서
+                // Thread를 종료시키는 경우를 방어하기 위해 while문 안에서 try catch를 잡는다.
+                //while(!shutdown || !queue.isEmpty())
+                /**
+                 * {@link #SHUTDOWN_TASK}와 같은 terminator를 사용하면
+                 * 1. Queue가 비어있는 상태를 보장하고, ThreadPool을 종료할 수 있기 때문에
+                 * (SHUTDOWN_TASK를 받으면 자동 종료가 되니까) Queue의 상태를 확인하지 않아도 된다.
+                 * 2. shutdown인지 확인 할 필요가 없어진다.
+                 * -> SHUTDOWN_TASK를 수행하면 뒤에 Job을 받기 전에 Thread가 종료되어 버리기 때문에
+                 * => 이제 shutdown은 {@link #shutdown()} 이 되었는데 Queue에 Job을 넣는것을 방어하기 위함으로만 사용된다.
+                 */
+                for(;;) {
+                    try{
                         final Runnable task = queue.take();
-                        task.run();
+                        //Shutdown task로 시그널을 보내면 Interrupt Signal을 보낼 이유도 없어진다.
+                        if(task == SHUTDOWN_TASK){
+                            break;
+                            /**
+                             * SHUTDOWN_TASK는 Thread가 한번만 받아도 return을 하기 때문에
+                             * 하나의 Thread가 여러번 받을 이유는 없다.
+                             * Thread갯수 만큼만 queue에 넣어주게 되면, 모든 Thread가 Shutdown에 들어가게 된다.
+                             */
+                        }else {
+                            task.run();
+                        }
+                    //Throwable이냐 Exception이냐? throw new Error로 사용자가 던져버리면 Exception으로는 잡지 못한다.
+                    }catch (Throwable throwable){
+                        /**Java 언어 spec상 던질수 없다고 하지만, 실제로는 Interrupted Exception을 던질 수 있다.
+                         * Exception의 타입을 E 와 같은 타입 파라미터로 던지게 되면 타입에 상관없이 모두 던질 수 있다.
+                         * {@link ThreadPool#doThrowUnsafely(Throwable)}
+                         * */
+                        if(!(throwable instanceof InterruptedException)){
+                            System.err.println("Unexpected Exception: ");
+                            throwable.printStackTrace();
+                        }
                     }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
                 }
-                //기다리는 작업이 실행중일때 interrupt가 발생할 수 있기 떄문에 예외
+                System.err.println("Shutting thread  '" + Thread.currentThread().getName() + '\'');
             });
 
             /**
@@ -55,8 +127,99 @@ public class ThreadPool implements Executor {
                 thread.start();
             }
         }
+        if(shutdown.get()){
+            throw new RejectedExecutionException();
+        }
+        /**
+         * shutdown이 아닌 것을 확인하고, queue에 command를 넣으려는데, 누가 shutdown()을 호출한다면?
+         * 그렇게 되면 SHUTDOWN이 추가되고 나서 queue에 Job이 추가되어서 Job이 실행이 되지 않는 경우가 발생할 수 있다.
+         * -> 순서를 바꿔 줘야한다.
+         * @problemNote
+         * 1. Thread A : execute(task)를 호출한다.
+         * 2. Thread A : shutdown == false임을 확인한다.
+         * 3. Thread B : shutdown() 을 호출한다.
+         * 4. Thread B : shutdown.compareAndSet(false, true)를 실행한다.
+         * 5. Thread B : queue에 SHUTDOWN_TASK를 모두 집어 넣는다.
+         * 6. Thread A : queue에 task를 추가한다. (!) --> task는 실행이 되지 않는다.
+         * @solutionList
+         * 1. 가장 단순한 방법 -> (2)false임을 확인하고, (6)queue에 task에 추가하는 부분을 lock을 잡아 처리하기
+         * B도 (3), (4), (5)를 같은 Lock을 잡아서 가져가면 된다. => 그렇게 하면 execute를 할때마다 lock이 잡힘
+         *
+         * */
         queue.add(command);
+        /**
+         * 여기서 문제가 발생할 수 있다.
+         * {@link ThreadPoolTest} 에서 Thread.sleep()함수 호출을 위해 Interrupted Exception을
+         * try catch로 잡는 경우, 각 thread의 기본 동작을 interrupt 시키는게 아니라, 해당 job의 interrupt를
+         * 발생시키기 때문에, thread가 shutdown이 되지 않는다.
+         */
     }
 
+    /**
+     * {@link }
+     * 사용자 입장에서는 execute를 하면 shutdown을 이후에 실행하더라도,
+     * 이미 execute한 Job은 모두 해결되고, shutdown이 되기를 바란다.
+     * 1. shutdown이 시작하면, 더 이상 Task를 받으면 안된다. {@link #execute(Runnable)}
+     *  - shutdown을 했는데 execute를 계속 받아주면, Queue가 Empty되지 않을 수 도 있다.
+     * 2. queue의 Job이 아예 없을 때 shutdown을 해야한다.
+     */
+    public void shutdown(){
+        //TODO: CAS로 started상태 확인하기
+        /**
+         * SHUTDOWN 여러번 호출 방지
+         */
+        if(shutdown.compareAndSet(false, true)){
+            for (int i = 0; i < threads.length; i++) {
+                queue.add(SHUTDOWN_TASK);
+            }
+        }
+        //TODO: Wait until the queue is completely drained.
+        /**
+         * 단순히 무한 Loop를 돌면서 Queue가 비어 있는지 확인할 수 도 있지만,
+         * 불필요한 자원을 소모 하게 된다.
+         */
+        /**
+         * thread가 100개인데, 하나씩 interrupt를 걸어주는 동안 또 자원이 낭비된다.
+         * 따라서, 아래처럼 한번씩 미리 interrupt를 시도시킨다.
+         * 따라서, 첫번째 thread가 shutdown될때 까지 기다리는 동안 두번쨰 thread도 이미 shutdown을 시작하게 된다.
+         * 결국, 동시적으로 thread가 종료를 시작하게 된다.
+         */
 
+        /*for (Thread thread : threads) {
+            thread.interrupt();
+        }*/
+        /**
+         * @implNote if문 안에 넣어버리게 되면, Thread1 shutdown()호출해서 Atomic 연산 성공,
+         * Thread2 shutdown()을 한번 더 호출 했을때 Atomic 연산이 실패하면서,
+         * Thread2는 thread.join()을 하지 않게 됨으로, Thread1의 shutdown으로 인해
+         * Thread2가 실제로 종료되기 전에 {@link #shutdown()}메소드가 return되게 되어 버린다.
+         * 하지만, 이것은 우리가 원하는 동작이 아니다. -> 우리는 thread가 모두 정상 종료되고나서, shutdown메소드가
+         * return되기를 원함
+         */
+        for (Thread thread : threads) {
+            //TODO: Thread를 정지하는 코드
+            //alive인 동안만 interrupt를 시도하며 돌면서 스레드가 멈추기를 기다리면, 불필요하게 CPU를 소모하게 된다.
+            //따라서 종료될때 알려주는 장치가 필요
+            do {
+                for (; ; ) {
+                    try {
+                        thread.join();
+                        //여기서 메소드 예외로 전파하지 않는 이유는 thread가 반 쯤 종료된 상태에서 예외를 전파시켜버리면
+                        //Thread Pool의 상태가 엉망이 되어버리기 때문이다.
+                    } catch (InterruptedException e) {
+                        //Do not propagate to prevent incomplete shutdown.
+                    }
+                    /*if(!thread.isAlive()){
+                        break;
+                    }
+                    thread.interrupt();*/
+                }
+            } while (thread.isAlive());
+        }
+        //doThrowUnsafely(new InterruptedException());
+    }
+
+    private static <E extends Throwable> void doThrowUnsafely(Throwable cause) throws E{
+        throw (E) cause;
+    }
 }
--- src/test/java/org/example/threadpool/ThreadPoolTest.java
+++ src/test/java/org/example/threadpool/ThreadPoolTest.java
@@ -11,30 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ThreadPoolTest {
 
-    //@Test
-    @RepeatedTest(1000) //재현 확률을 높이기 위한 테스트
+    @Test
+    //@RepeatedTest(1000) //재현 확률을 높이기 위한 테스트
     @DisplayName("가장 원시적인 ThreadPool Test")
     void submittedTasksAreExecuted() throws InterruptedException {
-        final Executor executor = new ThreadPool(2);
+        final ThreadPool executor = new ThreadPool(2);
         final int numTasks = 100;
         //Thread가 모두 끝날때 까지 기다리기 위한 CountDownLatch
         final CountDownLatch countDownLatch = new CountDownLatch(numTasks);
-        for (int i = 0; i < numTasks; i++) {
-            int finalI = i;
-            executor.execute(() -> {
-                System.err.println(Thread.currentThread().getId() + " executes a task" + finalI);
-                // 더 명확히 각 Thread가 어떻게 작업을 처리하는지 보기위한 Sleep()
-                //너무 짧으면 하나의 Thread로도 모두 처리가 가능해져 버린다.
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                countDownLatch.countDown();
-            });
+        try{
+            for (int i = 0; i < numTasks; i++) {
+                int finalI = i;
+                executor.execute(() -> {
+                    System.err.println(Thread.currentThread().getId() + " executes a task" + finalI);
+                    // 더 명확히 각 Thread가 어떻게 작업을 처리하는지 보기위한 Sleep()
+                    //너무 짧으면 하나의 Thread로도 모두 처리가 가능해져 버린다.
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        //throw new RuntimeException(e);
+                        //I don't care
+                    }
+                    //throw new InterruptedException();
+                    countDownLatch.countDown();
+                });
+            }
+            //latch가 0이 될때까지 기다린다.
+            countDownLatch.await();
+        }finally {
+           executor.shutdown();
         }
-        //latch가 0이 될때까지 기다린다.
-        countDownLatch.await();
     }
-
 }
